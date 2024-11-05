ade4ca7cf7a8081cc323c25298dca1f42bcdeb46
Bhavyamm <bhavyamm01@gmail.com>
2024-11-05 15:11:10 +0530

    Added assets, implemented sidebar

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React, { use } from 'react'
+
+const Meeting = ({ params }: { params: Promise<{ id: string }> }) => {
+    const { id } = use(params);
+    return (
+        <div>
+            Meeting Room: #{id}
+        </div>
+    )
+}
+
+export default Meeting
